6aab67434b834cc524eb85aa49bf8ede87793be2
saitotaishi <sai10tai41112@gmail.com>
2025-02-02 23:40:41 +0900

    デプロイ時のエラー解消

--- client-frontend/src/app/(dashboard)/companies/useCompaniesHooks.ts
+++ client-frontend/src/app/(dashboard)/companies/useCompaniesHooks.ts
@@ -16,6 +16,14 @@ interface Company {
   established: string;
 }
 
+interface Employee{
+  id: string,
+  companyID: string,
+  name: string,
+  affiliation: string,
+  position: string,
+  email: string,
+}
 
 export function useCompaniesHooks() {
   // const companies: Company[] = [
@@ -77,76 +85,76 @@ export function useCompaniesHooks() {
   //   },
   // ];
 
-  // const employees: Employee[] = [
-  //   // サンプル株式会社の従業員
-  //   {
-  //     id: "e1",
-  //     companyID: "1",
-  //     name: "山田 太郎",
-  //     affiliation: "空間マーケティング部 キュレーション課",
-  //     position: "エンジニア",
-  //     email: "taro.yamada@example.com",
-  //   },
-  //   {
-  //     id: "e2",
-  //     companyID: "1",
-  //     name: "佐藤 花子",
-  //     affiliation: "総合支援部",
-  //     position: "プロジェクトマネージャー",
-  //     email: "hanako.sato@example.com",
-  //   },
-  //   // テスト合同会社の従業員
-  //   {
-  //     id: "e3",
-  //     companyID: "2",
-  //     name: "鈴木 一郎",
-  //     affiliation: "パッケージイノベーション部",
-  //     position: "物流マネージャー",
-  //     email: "ichiro.suzuki@test.co.jp",
-  //   },
-  //   {
-  //     id: "e4",
-  //     companyID: "2",
-  //     name: "高橋 次郎",
-  //     affiliation: "パッケージイノベーション部",
-  //     position: "ECサイト担当",
-  //     email: "jiro.takahashi@test.co.jp",
-  //   },
-  //   // DEMO株式会社の従業員
-  //   {
-  //     id: "e5",
-  //     companyID: "3",
-  //     name: "田中 三郎",
-  //     affiliation: "パッケージイノベーション部",
-  //     position: "マーケティング担当",
-  //     email: "saburo.tanaka@demo.jp",
-  //   },
-  //   {
-  //     id: "e6",
-  //     companyID: "3",
-  //     name: "伊藤 四郎",
-  //     affiliation: "パッケージイノベーション部",
-  //     position: "コンサルタント",
-  //     email: "shiro.ito@demo.jp",
-  //   },
-  //   // ABCホールディングスの従業員
-  //   {
-  //     id: "e7",
-  //     companyID: "4",
-  //     name: "渡辺 五郎",
-  //     affiliation: "パッケージイノベーション部",
-  //     position: "製造部長",
-  //     email: "goro.watanabe@abc-holdings.co.jp",
-  //   },
-  //   {
-  //     id: "e8",
-  //     companyID: "4",
-  //     name: "中村 六郎",
-  //     affiliation: "パッケージイノベーション部",
-  //     position: "ITソリューションエンジニア",
-  //     email: "rokuro.nakamura@abc-holdings.co.jp",
-  //   },
-  // ];
+  const employees: Employee[] = [
+    // サンプル株式会社の従業員
+    {
+      id: "e1",
+      companyID: "1",
+      name: "山田 太郎",
+      affiliation: "空間マーケティング部 キュレーション課",
+      position: "エンジニア",
+      email: "taro.yamada@example.com",
+    },
+    {
+      id: "e2",
+      companyID: "1",
+      name: "佐藤 花子",
+      affiliation: "総合支援部",
+      position: "プロジェクトマネージャー",
+      email: "hanako.sato@example.com",
+    },
+    // テスト合同会社の従業員
+    {
+      id: "e3",
+      companyID: "2",
+      name: "鈴木 一郎",
+      affiliation: "パッケージイノベーション部",
+      position: "物流マネージャー",
+      email: "ichiro.suzuki@test.co.jp",
+    },
+    {
+      id: "e4",
+      companyID: "2",
+      name: "高橋 次郎",
+      affiliation: "パッケージイノベーション部",
+      position: "ECサイト担当",
+      email: "jiro.takahashi@test.co.jp",
+    },
+    // DEMO株式会社の従業員
+    {
+      id: "e5",
+      companyID: "3",
+      name: "田中 三郎",
+      affiliation: "パッケージイノベーション部",
+      position: "マーケティング担当",
+      email: "saburo.tanaka@demo.jp",
+    },
+    {
+      id: "e6",
+      companyID: "3",
+      name: "伊藤 四郎",
+      affiliation: "パッケージイノベーション部",
+      position: "コンサルタント",
+      email: "shiro.ito@demo.jp",
+    },
+    // ABCホールディングスの従業員
+    {
+      id: "e7",
+      companyID: "4",
+      name: "渡辺 五郎",
+      affiliation: "パッケージイノベーション部",
+      position: "製造部長",
+      email: "goro.watanabe@abc-holdings.co.jp",
+    },
+    {
+      id: "e8",
+      companyID: "4",
+      name: "中村 六郎",
+      affiliation: "パッケージイノベーション部",
+      position: "ITソリューションエンジニア",
+      email: "rokuro.nakamura@abc-holdings.co.jp",
+    },
+  ];
 
   const [open, setOpen] = useState<boolean>(false);
   const [selectedCompanyName, setSelectedCompanyName] = useState("");
@@ -182,7 +190,9 @@ export function useCompaniesHooks() {
   };
 
   const getEmployeesByCompanyId = (companyId: string) => {
+    //TODO デプロイ時にここでエラーが発生してしまうため。
     return employees.filter((employee) => employee.companyID === companyId);
+
   };
 
   return {
